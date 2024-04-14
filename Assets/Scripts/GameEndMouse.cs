fc47624fe71de8ab0f6274fbb7cc611adabc2cca
D_One <misharuk05@mail.ru>
2024-04-14 18:02:17 +0300

    Added

---
+++ Assets/Scripts/GameEndMouse.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class GameEndMouse : MonoBehaviour
+{
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+}
